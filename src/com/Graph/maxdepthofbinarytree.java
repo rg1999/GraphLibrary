c6224d8b508d7bef7d98b794158eac860cfa6423
Rishi Gupta <rishigupta046@gmail.com>
2019-10-15 23:19:51 +0530

    maxdepthofbinarytreeinjava

---
+++ src/com/Graph/maxdepthofbinarytree.java
@@ -0,0 +1,42 @@
+// Java program to find height of tree 
+
+// A binary tree node 
+class Node 
+{ 
+	int data; 
+	Node left, right; 
+
+	Node(int item) 
+	{ 
+		data = item; 
+		left = right = null; 
+	} 
+} 
+
+class BinaryTree 
+{ 
+	Node root; 
+
+	/* Compute the "maxDepth" of a tree -- the number of 
+	nodes along the longest path from the root node 
+	down to the farthest leaf node.*/
+	int maxDepth(Node node) 
+	{ 
+		if (node == null) 
+			return 0; 
+		else
+		{ 
+			/* compute the depth of each subtree */
+			int lDepth = maxDepth(node.left); 
+			int rDepth = maxDepth(node.right); 
+
+			/* use the larger one */
+			if (lDepth > rDepth) 
+				return (lDepth + 1); 
+			else
+				return (rDepth + 1); 
+		} 
+	} 
+	
+
+} 
